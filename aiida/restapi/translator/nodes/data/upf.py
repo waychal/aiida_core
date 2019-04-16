c36e3609017ae83dbc06a5a798e47c6fa25ad226
Snehal Kumbhar <snehal.kumbhar@epfl.ch>
2019-04-16 10:21:23 +0200

    fixed yapf error in restapi upf file

--- aiida/restapi/translator/nodes/data/upf.py
+++ aiida/restapi/translator/nodes/data/upf.py
@@ -14,6 +14,7 @@ Translator for upf data
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
+from aiida.restapi.translator.nodes.node import NodeTranslator
 from aiida.restapi.translator.nodes.data import DataTranslator
 from aiida.restapi.common.exceptions import RestInputValidationError
 
